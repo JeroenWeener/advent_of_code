f4201428511fd11050570be80bb359b8837d19aa
Jeroen Weener <jweener1996@gmail.com>
2022-12-06 22:43:26 +0100

    Add solutions for 2015, day 3, part 1 & 2

---
+++ 2015/dart/03/main.dart
@@ -0,0 +1,122 @@
+import 'dart:io';
+
+void main() {
+  final String input = File('2015/dart/03/input.txt').readAsStringSync();
+
+  final int answer1 = part1(input);
+  print(answer1);
+
+  final int answer2 = part2(input);
+  print(answer2);
+}
+
+/// --- Day 3: Perfectly Spherical Houses in a Vacuum ---
+///
+/// Santa is delivering presents to an infinite two-dimensional grid of houses.
+///
+/// He begins by delivering a present to the house at his starting location, and
+/// then an elf at the North Pole calls him via radio and tells him where to
+/// move next. Moves are always exactly one house to the north (^), south (v),
+/// east (>), or west (<). After each move, he delivers another present to the
+/// house at his new location.
+///
+/// However, the elf back at the north pole has had a little too much eggnog,
+/// and so his directions are a little off, and Santa ends up visiting some
+/// houses more than once. How many houses receive at least one present?
+///
+/// For example:
+///
+/// - > delivers presents to 2 houses: one at the starting location, and one to
+///   the east.
+/// - ^>v< delivers presents to 4 houses in a square, including twice to the
+///   house at his starting/ending location.
+/// - ^v^v^v^v^v delivers a bunch of presents to some very lucky children at
+///   only 2 houses.
+int part1(String input) {
+  final locations = {'0 0': null};
+
+  int santaX = 0;
+  int santaY = 0;
+
+  for (final direction in input.codeUnits) {
+    if (direction == '^'.codeUnits.first) {
+      santaY++;
+    } else if (direction == '>'.codeUnits.first) {
+      santaX++;
+    } else if (direction == '<'.codeUnits.first) {
+      santaX--;
+    } else {
+      santaY--;
+    }
+
+    locations['$santaX $santaY'] = null;
+  }
+
+  return locations.length;
+}
+
+/// --- Part Two ---
+///
+/// The next year, to speed up the process, Santa creates a robot version of
+/// himself, Robo-Santa, to deliver presents with him.
+///
+/// Santa and Robo-Santa start at the same location (delivering two presents to
+/// the same starting house), then take turns moving based on instructions from
+/// the elf, who is eggnoggedly reading from the same script as the previous
+/// year.
+///
+/// This year, how many houses receive at least one present?
+///
+/// For example:
+///
+/// - ^v delivers presents to 3 houses, because Santa goes north, and then
+///   Robo-Santa goes south.
+/// - ^>v< now delivers presents to 3 houses, and Santa and Robo-Santa end up
+///   back where they started.
+/// - ^v^v^v^v^v now delivers presents to 11 houses, with Santa going one
+///   direction and Robo-Santa going the other.
+int part2(String input) {
+  final santaLocations = {'0 0': null};
+  final roboLocations = {'0 0': null};
+
+  int santaX = 0;
+  int santaY = 0;
+  int roboX = 0;
+  int roboY = 0;
+
+  bool santasTurn = true;
+
+  for (final direction in input.codeUnits) {
+    if (santasTurn) {
+      if (direction == '^'.codeUnits.first) {
+        santaY++;
+      } else if (direction == '>'.codeUnits.first) {
+        santaX++;
+      } else if (direction == '<'.codeUnits.first) {
+        santaX--;
+      } else {
+        santaY--;
+      }
+
+      santaLocations['$santaX $santaY'] = null;
+    } else {
+      if (direction == '^'.codeUnits.first) {
+        roboY++;
+      } else if (direction == '>'.codeUnits.first) {
+        roboX++;
+      } else if (direction == '<'.codeUnits.first) {
+        roboX--;
+      } else {
+        roboY--;
+      }
+
+      roboLocations['$roboX $roboY'] = null;
+    }
+
+    santasTurn = !santasTurn;
+  }
+
+  santaLocations.addAll(roboLocations);
+
+  return santaLocations.length;
+}

d0affa640564507344d37924d990035e1a30604d
Jeroen Weener <jweener1996@gmail.com>
2022-12-21 00:07:05 +0100

    Add solutions for 2022, day 20, part 1 & 2

--- 2022/dart/20/main.dart
+++ 2022/dart/20/main.dart
@@ -6,39 +6,92 @@ void main() {
   int answer1 = part1(input);
   print(answer1);
 
-  int answer2 = part2(input);
+  BigInt answer2 = part2(input);
   print(answer2);
 }
 
 class RingElement {
-  RingElement({
-    required this.value,
-  });
+  RingElement(this.value);
 
   final int value;
   late RingElement predecessor;
   late RingElement successor;
 
-  RingElement getRelativeElement(int i) {
+  /// Return the [RingElement] [i] places from this.
+  ///
+  /// If [i] is larger than the size of the ring, this iterative algorithm will
+  /// pass this specific element at one point. This method is 'exclusive' in the
+  /// sense that it will not count this element when progressing.
+  ///
+  /// For example:
+  ///
+  ///   Ring: 6,1,2,3,4
+  ///   Starting from '6' in the ring, element i has value j:
+  ///     i: j
+  ///     1: 1
+  ///     2: 2
+  ///     3: 3
+  ///     4: 4
+  ///     5: 1
+  ///     6: 2
+  RingElement getRelativeElementExclusive(int i) {
+    return _getRelativeElementExclusive(this, i);
+  }
+
+  RingElement _getRelativeElementExclusive(RingElement starter, int i) {
     if (i == 0) {
       return this;
     }
+
     return i > 0
-        ? successor.getRelativeElement(--i)
-        : predecessor.getRelativeElement(++i);
+        ? successor._getRelativeElementExclusive(
+            starter, i - (successor == starter ? 0 : 1))
+        : predecessor._getRelativeElementExclusive(
+            starter, i + (predecessor == starter ? 0 : 1));
   }
 
+  /// Return the [RingElement] [i] places from this.
+  ///
+  /// If [i] is larger than the size of the ring, this iterative algorithm will
+  /// pass this specific element at one point. This method is 'inclusive' in the
+  /// sense that it will count this element when progressing.
+  ///
+  /// For example:
+  ///
+  ///   Ring: 6,1,2,3,4
+  ///   Starting from '6' in the ring, element i has value j:
+  ///     i: j
+  ///     1: 1
+  ///     2: 2
+  ///     3: 3
+  ///     4: 4
+  ///     5: 6
+  ///     6: 1
+  RingElement getRelativeElementInclusive(int i) {
+    if (i == 0) {
+      return this;
+    }
+
+    return i > 0
+        ? successor.getRelativeElementInclusive(--i)
+        : predecessor.getRelativeElementInclusive(++i);
+  }
+
+  /// Shift this element in its ring by [value] items.
+  ///
+  /// This procedure will not count the element itself when shifting, should
+  /// [value] exceed the size of the ring. See [getRelativeElementExclusive].
   void move() {
     if (value == 0) {
       return;
     }
 
     if (value > 0) {
-      RingElement newPredecessor = getRelativeElement(value);
+      RingElement newPredecessor = getRelativeElementExclusive(value);
       if (newPredecessor == this) {
         newPredecessor = successor;
       }
-      RingElement newSuccessor = newPredecessor.successor;
+      RingElement newSuccessor = newPredecessor.getRelativeElementExclusive(1);
       if (newSuccessor == this) {
         newSuccessor = successor;
       }
@@ -52,7 +105,7 @@ class RingElement {
     }
 
     if (value < 0) {
-      RingElement newSuccessor = getRelativeElement(value);
+      RingElement newSuccessor = getRelativeElementExclusive(value);
       if (newSuccessor == this) {
         newSuccessor = predecessor;
       }
@@ -70,32 +123,102 @@ class RingElement {
     }
   }
 
+  String printRing() {
+    return _printRing(predecessor);
+  }
+
   String _printRing(RingElement starter) {
     return starter == this
         ? '$value'
         : '$value,${successor._printRing(starter)}';
   }
-
-  String printRing() {
-    return _printRing(predecessor);
-  }
-
-  int _size(RingElement starter) {
-    if (starter == this) {
-      return 1;
-    }
-    return 1 + successor._size(starter);
-  }
-
-  int size() {
-    return _size(predecessor);
-  }
 }
 
+/// --- Day 20: Grove Positioning System ---
+///
+/// It's finally time to meet back up with the Elves. When you try to contact
+/// them, however, you get no reply. Perhaps you're out of range?
+///
+/// You know they're headed to the grove where the star fruit grows, so if you
+/// can figure out where that is, you should be able to meet back up with them.
+///
+/// Fortunately, your handheld device has a file (your puzzle input) that
+/// contains the grove's coordinates! Unfortunately, the file is encrypted -
+/// just in case the device were to fall into the wrong hands.
+///
+/// Maybe you can decrypt it?
+///
+/// When you were still back at the camp, you overheard some Elves talking about
+/// coordinate file encryption. The main operation involved in decrypting the
+/// file is called mixing.
+///
+/// The encrypted file is a list of numbers. To mix the file, move each number
+/// forward or backward in the file a number of positions equal to the value of
+/// the number being moved. The list is circular, so moving a number off one end
+/// of the list wraps back around to the other end as if the ends were
+/// connected.
+///
+/// For example, to move the 1 in a sequence like 4, 5, 6, 1, 7, 8, 9, the 1
+/// moves one position forward: 4, 5, 6, 7, 1, 8, 9. To move the -2 in a
+/// sequence like 4, -2, 5, 6, 7, 8, 9, the -2 moves two positions backward,
+/// wrapping around: 4, 5, 6, 7, 8, -2, 9.
+///
+/// The numbers should be moved in the order they originally appear in the
+/// encrypted file. Numbers moving around during the mixing process do not
+/// change the order in which the numbers are moved.
+///
+/// Consider this encrypted file:
+///
+///   1
+///   2
+///   -3
+///   3
+///   -2
+///   0
+///   4
+///
+///
+/// Mixing this file proceeds as follows:
+///
+///   Initial arrangement:
+///   1, 2, -3, 3, -2, 0, 4
+///
+///   1 moves between 2 and -3:
+///   2, 1, -3, 3, -2, 0, 4
+///
+///   2 moves between -3 and 3:
+///   1, -3, 2, 3, -2, 0, 4
+///
+///   -3 moves between -2 and 0:
+///   1, 2, 3, -2, -3, 0, 4
+///
+///   3 moves between 0 and 4:
+///   1, 2, -2, -3, 0, 3, 4
+///
+///   -2 moves between 4 and 1:
+///   1, 2, -3, 0, 3, 4, -2
+///
+///   0 does not move:
+///   1, 2, -3, 0, 3, 4, -2
+///
+///   4 moves between -3 and 0:
+///   1, 2, -3, 4, 0, 3, -2
+///
+///
+/// Then, the grove coordinates can be found by looking at the 1000th, 2000th,
+/// and 3000th numbers after the value 0, wrapping around the list as necessary.
+/// In the above example, the 1000th number after 0 is 4, the 2000th is -3, and
+/// the 3000th is 2; adding these together produces 3.
+///
+/// Mix your encrypted file exactly once. What is the sum of the three numbers
+/// that form the grove coordinates?
 int part1(List<String> input) {
+  /// Store [RingElement]s in a list. This list will not change order throughout
+  /// solving the puzzle.
   List<RingElement> elements =
-      input.map((value) => RingElement(value: int.parse(value))).toList();
+      input.map((value) => RingElement(int.parse(value))).toList();
 
+  /// Populate every [RingElement] with its predecessor and successor.
   for (int i = 0; i < elements.length; i++) {
     RingElement element = elements[i];
     RingElement formerElement = elements[(i - 1) % elements.length];
@@ -104,15 +227,117 @@ int part1(List<String> input) {
     element.successor = laterElement;
   }
 
+  /// Mix the file by moving every element in order.
   elements.forEach((element) => element.move());
 
+  /// Find the zero element.
   RingElement zero = elements.firstWhere((element) => element.value == 0);
 
+  /// Calculate the sum of the grove coordinates. When finding the grove
+  /// coordinates, we count elements inclusive.
   return [1000, 2000, 3000]
-      .map((groveCoordinate) => zero.getRelativeElement(groveCoordinate).value)
+      .map((groveCoordinate) =>
+          zero.getRelativeElementInclusive(groveCoordinate).value)
       .reduce((a, b) => a + b);
 }
 
-int part2(List<String> input) {
-  return -1;
+/// --- Part Two ---
+///
+/// The grove coordinate values seem nonsensical. While you ponder the mysteries
+/// of Elf encryption, you suddenly remember the rest of the decryption routine
+/// you overheard back at camp.
+///
+/// First, you need to apply the decryption key, 811589153. Multiply each number
+/// by the decryption key before you begin; this will produce the actual list of
+/// numbers to mix.
+///
+/// Second, you need to mix the list of numbers ten times. The order in which
+/// the numbers are mixed does not change during mixing; the numbers are still
+/// moved in the order they appeared in the original, pre-mixed list. (So, if
+/// -3 appears fourth in the original list of numbers to mix, -3 will be the
+/// fourth number to move during each round of mixing.)
+///
+/// Using the same example as above:
+///
+///   Initial arrangement:
+///   811589153, 1623178306, -2434767459, 2434767459, -1623178306, 0, 3246356612
+///
+///   After 1 round of mixing:
+///   0, -2434767459, 3246356612, -1623178306, 2434767459, 1623178306, 811589153
+///
+///   After 2 rounds of mixing:
+///   0, 2434767459, 1623178306, 3246356612, -2434767459, -1623178306, 811589153
+///
+///   After 3 rounds of mixing:
+///   0, 811589153, 2434767459, 3246356612, 1623178306, -1623178306, -2434767459
+///
+///   After 4 rounds of mixing:
+///   0, 1623178306, -2434767459, 811589153, 2434767459, 3246356612, -1623178306
+///
+///   After 5 rounds of mixing:
+///   0, 811589153, -1623178306, 1623178306, -2434767459, 3246356612, 2434767459
+///
+///   After 6 rounds of mixing:
+///   0, 811589153, -1623178306, 3246356612, -2434767459, 1623178306, 2434767459
+///
+///   After 7 rounds of mixing:
+///   0, -2434767459, 2434767459, 1623178306, -1623178306, 811589153, 3246356612
+///
+///   After 8 rounds of mixing:
+///   0, 1623178306, 3246356612, 811589153, -2434767459, 2434767459, -1623178306
+///
+///   After 9 rounds of mixing:
+///   0, 811589153, 1623178306, -2434767459, 3246356612, 2434767459, -1623178306
+///
+///   After 10 rounds of mixing:
+///   0, -2434767459, 1623178306, 3246356612, -1623178306, 2434767459, 811589153
+///
+///
+/// The grove coordinates can still be found in the same way. Here, the 1000th
+/// number after 0 is 811589153, the 2000th is 2434767459, and the 3000th is
+/// -1623178306; adding these together produces 1623178306.
+///
+/// Apply the decryption key and mix your encrypted file ten times. What is the
+/// sum of the three numbers that form the grove coordinates?
+BigInt part2(List<String> input) {
+  int decryptionKey = 811589153;
+
+  /// Create the ring again. Instead of applying the decryption key directly, we
+  /// use mathematics to shrink the numbers dramatically.
+  ///
+  /// A [RingElement] with a value that is equal to the size of the ring minus 1
+  /// will not move. We use this fact by shrinking first the decryption key and
+  /// later the resulting value in the element by applying the modulo operator.
+  List<RingElement> elements = input
+      .map((value) => RingElement(
+          ((decryptionKey % (input.length - 1)) * int.parse(value)) %
+              (input.length - 1)))
+      .toList();
+
+  /// Populate every [RingElement] with its predecessor and successor again.
+  for (int i = 0; i < elements.length; i++) {
+    RingElement element = elements[i];
+    RingElement formerElement = elements[(i - 1) % elements.length];
+    RingElement laterElement = elements[(i + 1) % elements.length];
+    element.predecessor = formerElement;
+    element.successor = laterElement;
+  }
+
+  /// This time, we apply the mix 10 times.
+  for (int i = 0; i < 10; i++) {
+    elements.forEach((element) => element.move());
+  }
+
+  /// Find the zero element.
+  RingElement zero = elements.firstWhere((element) => element.value == 0);
+
+  /// Find the 3 elements corresponding to the grove coordinates. Then, find
+  /// their index in [elements] and calculate the actual values by fetching the
+  /// original numbers and applying the decryption key.
+  return [1000, 2000, 3000]
+      .map((groveCoordinate) =>
+          BigInt.parse(input[elements
+              .indexOf(zero.getRelativeElementInclusive(groveCoordinate))]) *
+          BigInt.from(decryptionKey))
+      .reduce((a, b) => a + b);
 }

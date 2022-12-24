ae55dbba854a8643a4cb5d65c284b34a7ce4f899
Jeroen Weener <jweener1996@gmail.com>
2022-12-24 14:51:41 +0100

    Add solution for 2022, day 15, part 2

--- 2022/dart/15/main.dart
+++ 2022/dart/15/main.dart
@@ -1,5 +1,4 @@
 import 'dart:io';
-import 'dart:math';
 
 void main() {
   List<String> input = File('2022/dart/15/input.txt').readAsLinesSync();
@@ -50,6 +49,15 @@ class Sensor {
     return position.distanceFrom(this.position) <= radius;
   }
 
+  /// Return the last x-coordinate in the [y]-slice of the coverage of this
+  /// sensor.
+  ///
+  /// This method only returns a valid result if [y] is within the coverage
+  /// area.
+  int lastXInY(int y) {
+    return position.x + radius - (position.y - y).abs();
+  }
+
   @override
   String toString() {
     return 'S(${position} $radius)';
@@ -87,12 +95,142 @@ List<Position> extractBeacons(List<String> input) {
             .map((numberString) => int.parse(numberString!))
             .toList(),
       )
-      .map(
-        (numberList) => Position(numberList[0], numberList[1]),
-      )
+      .map((numberList) => Position(numberList[0], numberList[1]))
       .toList();
 }
 
+/// --- Day 15: Beacon Exclusion Zone ---
+///
+/// You feel the ground rumble again as the distress signal leads you to a large
+/// network of subterranean tunnels. You don't have time to search them all, but
+/// you don't need to: your pack contains a set of deployable sensors that you
+/// imagine were originally built to locate lost Elves.
+///
+/// The sensors aren't very powerful, but that's okay; your handheld device
+/// indicates that you're close enough to the source of the distress signal to
+/// use them. You pull the emergency sensor system out of your pack, hit the big
+/// button on top, and the sensors zoom off down the tunnels.
+///
+/// Once a sensor finds a spot it thinks will give it a good reading, it
+/// attaches itself to a hard surface and begins monitoring for the nearest
+/// signal source beacon. Sensors and beacons always exist at integer
+/// coordinates. Each sensor knows its own position and can determine the
+/// position of a beacon precisely; however, sensors can only lock on to the one
+/// beacon closest to the sensor as measured by the Manhattan distance. (There
+/// is never a tie where two beacons are the same distance to a sensor.)
+///
+/// It doesn't take long for the sensors to report back their positions and
+/// closest beacons (your puzzle input). For example:
+///
+///   Sensor at x=2, y=18: closest beacon is at x=-2, y=15
+///   Sensor at x=9, y=16: closest beacon is at x=10, y=16
+///   Sensor at x=13, y=2: closest beacon is at x=15, y=3
+///   Sensor at x=12, y=14: closest beacon is at x=10, y=16
+///   Sensor at x=10, y=20: closest beacon is at x=10, y=16
+///   Sensor at x=14, y=17: closest beacon is at x=10, y=16
+///   Sensor at x=8, y=7: closest beacon is at x=2, y=10
+///   Sensor at x=2, y=0: closest beacon is at x=2, y=10
+///   Sensor at x=0, y=11: closest beacon is at x=2, y=10
+///   Sensor at x=20, y=14: closest beacon is at x=25, y=17
+///   Sensor at x=17, y=20: closest beacon is at x=21, y=22
+///   Sensor at x=16, y=7: closest beacon is at x=15, y=3
+///   Sensor at x=14, y=3: closest beacon is at x=15, y=3
+///   Sensor at x=20, y=1: closest beacon is at x=15, y=3
+///
+///
+/// So, consider the sensor at 2,18; the closest beacon to it is at -2,15. For
+/// the sensor at 9,16, the closest beacon to it is at 10,16.
+///
+/// Drawing sensors as S and beacons as B, the above arrangement of sensors and
+/// beacons looks like this:
+///
+///                  1    1    2    2
+///        0    5    0    5    0    5
+///    0 ....S.......................
+///    1 ......................S.....
+///    2 ...............S............
+///    3 ................SB..........
+///    4 ............................
+///    5 ............................
+///    6 ............................
+///    7 ..........S.......S.........
+///    8 ............................
+///    9 ............................
+///   10 ....B.......................
+///   11 ..S.........................
+///   12 ............................
+///   13 ............................
+///   14 ..............S.......S.....
+///   15 B...........................
+///   16 ...........SB...............
+///   17 ................S..........B
+///   18 ....S.......................
+///   19 ............................
+///   20 ............S......S........
+///   21 ............................
+///   22 .......................B....
+///
+///
+/// This isn't necessarily a comprehensive map of all beacons in the area,
+/// though. Because each sensor only identifies its closest beacon, if a sensor
+/// detects a beacon, you know there are no other beacons that close or closer
+/// to that sensor. There could still be beacons that just happen to not be the
+/// closest beacon to any sensor. Consider the sensor at 8,7:
+///
+///                  1    1    2    2
+///        0    5    0    5    0    5
+///   -2 ..........#.................
+///   -1 .........###................
+///    0 ....S...#####...............
+///    1 .......#######........S.....
+///    2 ......#########S............
+///    3 .....###########SB..........
+///    4 ....#############...........
+///    5 ...###############..........
+///    6 ..#################.........
+///    7 .#########S#######S#........
+///    8 ..#################.........
+///    9 ...###############..........
+///   10 ....B############...........
+///   11 ..S..###########............
+///   12 ......#########.............
+///   13 .......#######..............
+///   14 ........#####.S.......S.....
+///   15 B........###................
+///   16 ..........#SB...............
+///   17 ................S..........B
+///   18 ....S.......................
+///   19 ............................
+///   20 ............S......S........
+///   21 ............................
+///   22 .......................B....
+///
+///
+/// This sensor's closest beacon is at 2,10, and so you know there are no
+/// beacons that close or closer (in any positions marked #).
+///
+/// None of the detected beacons seem to be producing the distress signal, so
+/// you'll need to work out where the distress beacon is by working out where it
+/// isn't. For now, keep things simple by counting the positions where a beacon
+/// cannot possibly be along just a single row.
+///
+/// So, suppose you have an arrangement of beacons and sensors like in the
+/// example above and, just in the row where y=10, you'd like to count the
+/// number of positions a beacon cannot possibly exist. The coverage from all
+/// sensors near that row looks like this:
+///
+///                    1    1    2    2
+///          0    5    0    5    0    5
+///    9 ...#########################...
+///   10 ..####B######################..
+///   11 .###S#############.###########.
+///
+///
+/// In this example, in the row where y=10, there are 26 positions where a
+/// beacon cannot be present.
+///
+/// Consult the report from the sensors you just deployed. In the row where
+/// y=2000000, how many positions cannot contain a beacon?
 int part1(List<String> input) {
   List<Sensor> sensors = extractSensors(input);
   List<Position> beacons = extractBeacons(input);
@@ -123,49 +261,50 @@ int part1(List<String> input) {
   return coveredLocations;
 }
 
+/// --- Part Two ---
+///
+/// Your handheld device indicates that the distress signal is coming from a
+/// beacon nearby. The distress beacon is not detected by any sensor, but the
+/// distress beacon must have x and y coordinates each no lower than 0 and no
+/// larger than 4000000.
+///
+/// To isolate the distress beacon's signal, you need to determine its tuning
+/// frequency, which can be found by multiplying its x coordinate by 4000000 and
+///  adding its y coordinate.
+///
+/// In the example above, the search space is smaller: instead, the x and y
+/// coordinates can each be at most 20. With this reduced search area, there is
+/// only a single position that could have a beacon: x=14, y=11. The tuning
+/// frequency for this distress beacon is 56000011.
+///
+/// Find the only possible position for the distress beacon. What is its tuning
+/// frequency?
 int part2(List<String> input) {
   List<Sensor> sensors = extractSensors(input);
   List<Position> beacons = extractBeacons(input);
 
-  int sizeRestriction = 4000000;
+  int size = 4000000;
 
-  int minX = sensors
-      .map((sensor) => sensor.position.x - sensor.radius)
-      .reduce((a, b) => a < b ? a : b);
-  minX = max(0, minX);
+  for (int y = 0; y < size; y++) {
+    for (int x = 0; x < size; x++) {
+      int newX = x;
 
-  int maxX = sensors
-      .map((sensor) => sensor.position.x + sensor.radius)
-      .reduce((a, b) => a > b ? a : b);
-  maxX = min(sizeRestriction, maxX);
-
-  int minY = sensors
-      .map((sensor) => sensor.position.y - sensor.radius)
-      .reduce((a, b) => a < b ? a : b);
-  minY = max(0, minY);
-
-  int maxY = sensors
-      .map((sensor) => sensor.position.y + sensor.radius)
-      .reduce((a, b) => a > b ? a : b);
-  maxY = min(sizeRestriction, maxY);
-
-  for (int x = minX; x <= maxX; x++) {
-    int progress = (x / (maxX - minX) * 100).toInt();
-    if (progress % 1 == 0) {
-      print(progress);
-    }
-    for (int y = minY; y <= maxY; y++) {
-      Position position = Position(x, y);
-      bool covered = sensors
-          .map((sensor) => sensor.coversPosition(position))
-          .any((isCovering) => isCovering);
-
-      if (!covered) {
-        bool containsBeacon = beacons.any((beacon) => beacon == position);
-        if (!containsBeacon) {
-          print(position);
-          return (position.x * sizeRestriction + position.y).abs();
+      // 'Swing' from current x to the last x-coordinate of the sensors,
+      // skipping evaluation of many x-coordinates in the process.
+      while (sensors.any((sensor) => sensor.coversPosition(Position(x, y)))) {
+        for (Sensor sensor in sensors) {
+          if (sensor.coversPosition(Position(x, y))) {
+            newX = sensor.lastXInY(y) + 1;
+          }
         }
+        x = newX;
+      }
+
+      // If the last swing resulted in an x within our evaluation bounds, and
+      // the position is not a known beacon, it must be the beacon we are
+      // looking for.
+      if (x < size && !beacons.any((beacon) => beacon == Position(x, y))) {
+        return 4000000 * x + y;
       }
     }
   }
